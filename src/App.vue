2a1750e03cce53f4798c10e3231cd55a444364c3
Zoya Volkova <zoearkhipova@gmail.com>
2021-12-05 17:30:24 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <file-tree></file-tree>
+</template>
+
+<script>
+import fileTree from './components/FileTree.vue'
+
+export default {
+  name: 'app',
+  components: {
+    fileTree,
+  },
+}
+</script>
